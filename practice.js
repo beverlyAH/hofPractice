3e59ee46f44e59c595f189a3888a514562717121
Beverly Hernandez <beverlyandez@gmail.com>
2021-08-13 14:02:27 -0400

    add new prompts

--- practice.js
+++ practice.js
@@ -4,19 +4,16 @@
  *
  */
 
-// use _.each to double each number in the array.
-var doubleNumbers = function(numbers) {
+// use _.each to create a copy of the given array.
+var moreFruits = function(fruits) {
 
-}
+};
 
 // use _.each to traverse the number array and determine
 // which are multiples of five.
 var multiplesOfFive = function(numbers) {
 
-}
-
-
-
+};
 
 /*
  *
@@ -24,19 +21,21 @@ var multiplesOfFive = function(numbers) {
  *
  */
 
-// use _.filter to return the fruits array with only lemon.
-var onlyLemons = function(fruits) {
+// use _.filter to return the fruits array with only the desired fruit.
+var onlyOneFruit = function(fruits, targetFruit) {
 
-}
+};
 
-var startsWithP = function(fruits) {
+// use _.filter to return the fruits array with only fruits
+// starting with the letter 'P'
+var startsWith = function(fruits, letter) {
 
-}
+};
 
 // return a filtered array containing only cookie-type desserts.
-var cookiesOnly = (desserts) => {
+var cookiesOnly = function(desserts) {
 
-}
+};
 
 /*
  *
@@ -46,24 +45,27 @@ var cookiesOnly = (desserts) => {
 
 // return the total price of all products.
 var sumTotal = function(products) {
-
-}
+  
+};
 
 // return an object consisting of dessert types and how many of each.
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
 var dessertCategories = function(desserts) {
 
-}
+};
 
-/*
-Use _.reduce to return a nested array, consisting of arrays contained two elements;
-the first representing an ingredient, and the second how many times it appears across
-all recipes.
-Example: [['sugar', 5], ['flour', 4], ['water', 3]]
-*/
-var ingredientInventory = function(desserts) {
+// given an array of movie data objects,return an array containing
+// movies that came out between 1990 and 2000.
+var ninetiesKid = function(movies) {
+  
+};
 
-}
+// return an boolean stating if there exists a movie with a shorter
+// runtime than your time limit.
+// timeLimit is an integer representing a number of minutes
+var movieNight = function(movies, timeLimit) {
+
+};
 
 /*
  *
@@ -71,18 +73,32 @@ var ingredientInventory = function(desserts) {
  *
  */
 
-// use _.map to return an array of items with their sale prices. round any decimals to two places.
+// use _.map to return an array of items with their sale prices, with a new property
+// containing the sale price. round any decimals to two places.
 /* example output:
   var salePrices = applyCoupon(grocery, 0.20);
   [
     {
       id: 1,
       product: 'Pork - Loin, Boneless',
-      price: 6.90 // original price 8.63
+      price: 8.63
+      salePrice: 6
     }
   ];
 */
-  var applyCoupon = function(grocery, coupon) {
+var applyCoupon = function(grocery, coupon) {
+    
+};
 
-  }
+// given an array of strings, use _.map to return a new array containing all
+// strings converted to uppercase letters.
+var upperCaseFruits = function(fruits) {
 
+};
+
+// given an array of dessert objects, return a new array of objects
+// that have a new "glutenFree" property, with a boolean value.
+// TIP: Items that contain flour are not gluten-free.
+var glutenFree = function(desserts) {
+
+};
